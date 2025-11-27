71179b8e1106655781fd9712af7856da70052aff
Jin Yt <815606833@qq.com>
2025-11-27 14:44:30 +0800

    Add files via upload

--- models/model_factory.py
+++ models/model_factory.py
@@ -4,36 +4,8 @@
 from models.PMNQP import PMNQP
 
 def get_model(config, attributes=None, classes=None, offset=None, dset=None):
-    if config.model_name == 'ThreeBranch':
-        model = ThreeBranchPromptModel(config, dset=dset)
-    elif config.model_name == 'CompBranch':
-        model = CompositionPromptModel(config, dset=dset)
-    elif config.model_name == 'MNSingleA':
-        model = MNSingleA(config, dset=dset)
-    elif config.model_name == 'PNPSingleA':
-        model = PNPSingleA(config, dset=dset)
-    elif config.model_name == 'cluspro':
-        model = cluspro(config, attributes=attributes, classes=classes, offset=offset)
-    elif config.model_name == 'DualOT':
-        model = DualOT(config, dset=dset)
-    elif config.model_name == 'ExtendThreeBranch':
-        model = ExtendThreeBranch(config, dset=dset)
-    elif config.model_name == 'ExtendThreeBranchWithPatchPrototypes':
-        model = ExtendThreeBranchWithPatchPrototypes(config, dset=dset)
-    elif config.model_name == 'ExtendThreeBranchWithMemory':
-        model = ExtendThreeBranchWithMemory(config, dset=dset)
-    elif config.model_name == 'ExtendThreeBranchWithOHEM':
-        model = ExtendThreeBranchWithOHEM(config, dset=dset)
-    elif config.model_name == 'ExtendThreeBranchTaylorCE':
-        model = ExtendThreeBranchTaylorCE(config, dset=dset)
-    elif config.model_name == 'ExtendThreeBranchWithMoEAdapter':
-        model = ExtendThreeBranchWithMoEAdapter(config, dset=dset)
-    elif config.model_name == 'ExtendThreeBranchWithPatchPrototypesMoEAdapter':
-        model = ExtendThreeBranchWithPatchPrototypesMoEAdapter(config, dset=dset)
-    elif config.model_name == 'ExtendThreeBranchWithHyperNetworkPrototypes':
-        model = ExtendThreeBranchWithHyperNetworkPrototypes(config, dset=dset)
-    elif config.model_name == 'ExtendThreeBranchWithHyperNetworkMoEAdapter':
-        model = ExtendThreeBranchWithHyperNetworkMoEAdapter(config, dset=dset)
+    if config.model_name == 'PMNQP':
+        model = PMNQP(config, dset=dset)
     else:
         raise NotImplementedError(
             "Error: Unrecognized Model Name {:s}.".format(
